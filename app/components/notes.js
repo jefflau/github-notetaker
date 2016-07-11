a762bdeda98f8853cc18b10593cfc9f29ceab62d
Jeff Lau <jeff-lau@live.com>
2016-07-11 18:18:15 +0800

    Updated to latest version of react native

--- app/components/notes.js
+++ app/components/notes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 import {
   ListView,
   View,
@@ -31,7 +31,12 @@ const styles = StyleSheet.create({
   }
 })
 
-//github-notetaker-571fe.firebaseio.com
+var config = {
+  apiKey: "AIzaSyDuuVVDcmRAkKr11CKm_EuJf8cweTEUvI8",
+  authDomain: "github-notetaker-571fe.firebaseapp.com",
+  databaseURL: "https://github-notetaker-571fe.firebaseio.com",
+  storageBucket: "github-notetaker-571fe.appspot.com",
+};
 
 const Footer = () => (
   <View style={styles.footer}>
@@ -41,10 +46,58 @@ const Footer = () => (
 )
 
 class Notes extends React.Component {
+  constructor(props){
+    super(props);
+
+    firebase.initializeApp(config);
+
+    // var database = firebase.database();
+
+    console.log('firebase', firebase)
+    let db = firebase.database();
+
+    db.ref('config').set({
+      key: 'something data'
+    }).catch((err)=> console.log(err))
+
+    let notesRef = db.ref('usernames');
+
+    notesRef.push({
+      title: 'something'
+    });
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([{note: 'blah'}, {note: 'blah blah'}])
+    }
+  }
+
+  componentDidMount() {
+    this.listenForNotes();
+  }
+
+  listenForNotes() {
+
+    notesRef.on('value', function(snapshot) {
+      snapshot.forEach(child => {
+        items.push({
+          note: child.val().note,
+          _key: child.key()
+        })
+      });
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+    });
+
+  }
   render() {
     return (
       <View style={styles.container}>
-        <ListView />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData.note}</Text>}
+
+        />
         <Footer />
       </View>
     )
